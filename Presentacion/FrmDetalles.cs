b0a75fc1a262cea7316b0f56e63d25a45826642f
Tomas <tomasliendo00@gmail.com>
2025-06-23 21:55:37 -0300

    fin.

--- Presentacion/FrmDetalles.cs
+++ Presentacion/FrmDetalles.cs
@@ -25,6 +25,7 @@ namespace EmpresaNorte.Presentacion
         AccesoDatos accesoDatos;
         Modo accion;
         Empleado oEmpleado;
+        int aux;
         public FrmDetalles(Modo modo, Empleado empleado)
         {
             InitializeComponent();
@@ -42,6 +43,7 @@ namespace EmpresaNorte.Presentacion
             if(accion == Modo.NUEVO)
             {
                 oEmpleado = new Empleado();
+                aux = 0;
             }
             else
             {
@@ -54,11 +56,15 @@ namespace EmpresaNorte.Presentacion
                 txtEmail.Text = oEmpleado.Email.ToString();
                 cboSucursal.SelectedIndex = oEmpleado.Sucursal.ID;
                 cboTipoEmpleado.SelectedIndex = oEmpleado.TipoEmpleado.ID;
-                //cboSucursal.SelectedValue = oEmpleado.Sucursal.ID;
-                //cboTipoEmpleado.SelectedValue = oEmpleado.TipoEmpleado.ID;
                 dtpFechaIng.Value = oEmpleado.FechaIngreso;
             }
-            if (accion == Modo.VER || accion == Modo.BORRAR)
+
+            //if(accion == Modo.EDITAR)
+            //{
+            //    aux = 2;
+            //}
+
+            if (accion == Modo.VER)
             {
                 txtID.Enabled = false;
                 txtNombre.Enabled = false;
@@ -70,6 +76,22 @@ namespace EmpresaNorte.Presentacion
                 cboSucursal.Enabled = false;
                 cboTipoEmpleado.Enabled = false;
                 dtpFechaIng.Enabled = false;
+                //aux = 1;
+            }
+
+            if (accion == Modo.BORRAR)
+            {
+                txtID.Enabled = false;
+                txtNombre.Enabled = false;
+                txtApellido.Enabled = false;
+                txtDNI.Enabled = false;
+                dtpFechaNac.Enabled = false;
+                txtTelefono.Enabled = false;
+                txtEmail.Enabled = false;
+                cboSucursal.Enabled = false;
+                cboTipoEmpleado.Enabled = false;
+                dtpFechaIng.Enabled = false;
+                //aux = 3;
             }
         }
 
@@ -145,9 +167,9 @@ namespace EmpresaNorte.Presentacion
                 cboTipoEmpleado.Focus();
                 return false;
             }
-            else if (!txtNombre.Text.All(char.IsLetter) || !txtApellido.Text.All(char.IsLetter))
+            else if (!txtNombre.Text.Replace(" ", "").All(char.IsLetter) || !txtApellido.Text.Replace(" ", "").All(char.IsLetter))
             {
-                MessageBox.Show("El nombre y el apellido solo pueden contener letras.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                MessageBox.Show("El nombre y el apellido solo pueden contener letras y espacios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 return false;
             }
             else if (!txtDNI.Text.All(char.IsNumber))
@@ -176,59 +198,171 @@ namespace EmpresaNorte.Presentacion
             Empleado empleado = new Empleado();
             empleado.Nombres = txtNombre.Text;
             empleado.Apellidos = txtApellido.Text;
-            empleado.DNI = txtDNI.Text; 
+            empleado.DNI = txtDNI.Text;
             empleado.FechaNacimiento = dtpFechaNac.Value;
-            empleado.Telefono = txtTelefono.Text; 
+            empleado.Telefono = txtTelefono.Text;
             empleado.Email = txtEmail.Text;
-
-            // Reemplazar estas líneas:
-            //empleado.Sucursal = Convert.ToInt32(cboSucursal.SelectedValue);
-            //empleado.TipoEmpleado = Convert.ToInt32(cboTipoEmpleado.SelectedValue);
-
-            // Por estas líneas:
             empleado.Sucursal = new Sucursal { ID = Convert.ToInt32(cboSucursal.SelectedValue) };
             empleado.TipoEmpleado = new TipoEmpleado { ID = Convert.ToInt32(cboTipoEmpleado.SelectedValue) };
             empleado.FechaIngreso = dtpFechaIng.Value;
 
-            string consultaSQL = "SET DATEFORMAT DMY; " +
-                                 "INSERT INTO EMPLEADOS VALUES " +
-                                 "(@nombre," +
-                                 " @apellido," +
-                                 " @sucursal," +
-                                 " @tipoEmpleado," +
-                                 " @dni," +
-                                 " @telefono," +
-                                 " @email," +
-                                 " @fechaNacimiento," +
-                                 " @fechaIngreso)";
-
             List<Parametro> lEmpleados = new List<Parametro>();
-
             lEmpleados.Add(new Parametro("@nombre", empleado.Nombres));
             lEmpleados.Add(new Parametro("@apellido", empleado.Apellidos));
             lEmpleados.Add(new Parametro("@dni", empleado.DNI));
             lEmpleados.Add(new Parametro("@fechaNacimiento", empleado.FechaNacimiento));
             lEmpleados.Add(new Parametro("@telefono", empleado.Telefono));
             lEmpleados.Add(new Parametro("@email", empleado.Email));
-            lEmpleados.Add(new Parametro("@sucursal", empleado.Sucursal));
-            lEmpleados.Add(new Parametro("@tipoEmpleado", empleado.TipoEmpleado));
+            lEmpleados.Add(new Parametro("@sucursal", empleado.Sucursal.ID));
+            lEmpleados.Add(new Parametro("@tipoEmpleado", empleado.TipoEmpleado.ID));
             lEmpleados.Add(new Parametro("@fechaIngreso", empleado.FechaIngreso));
+
+            string consultaSQL;
+
+            if (accion == Modo.NUEVO)
+            {
+                consultaSQL = "SET DATEFORMAT DMY; " +
+                              "INSERT INTO EMPLEADOS VALUES " +
+                              "(@nombre, @apellido, @sucursal, @tipoEmpleado, @dni, @telefono, @email, @fechaNacimiento, @fechaIngreso)";
+            }
+            else if (accion == Modo.EDITAR)
+            {
+                consultaSQL = "SET DATEFORMAT DMY; " +
+                              "UPDATE EMPLEADOS SET " +
+                              "nombre = @nombre, " +
+                              "apellido = @apellido, " +
+                              "id_sucursal = @sucursal, " +
+                              "id_tipo_empleado = @tipoEmpleado, " +
+                              "dni = @dni, " +
+                              "telefono = @telefono, " +
+                              "email = @email, " +
+                              "fecha_nac = @fechaNacimiento, " +
+                              "fecha_ingreso = @fechaIngreso " +
+                              "WHERE id_empleado = @id";
+                lEmpleados.Add(new Parametro("@id", oEmpleado.ID));
+            }
+            else
+            {
+                // Para BORRAR o VER no se hace nada aquí
+                return;
+            }
+
 
             if (accesoDatos.ActualizarBD(consultaSQL, lEmpleados) > 0)
             {
-                MessageBox.Show("Empleado cargado con exito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Exclamation,MessageBoxDefaultButton.Button1);
+                MessageBox.Show(
+                    accion == Modo.NUEVO ? "Empleado cargado con éxito." : "Empleado actualizado con éxito.",
+                    "Éxito",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation,
+                    MessageBoxDefaultButton.Button1
+                );
                 this.Close();
             }
         }
 
+        //private void cargarEmpleado()
+        //{
+        //    Empleado empleado = new Empleado();
+        //    empleado.Nombres = txtNombre.Text;
+        //    empleado.Apellidos = txtApellido.Text;
+        //    empleado.DNI = txtDNI.Text; 
+        //    empleado.FechaNacimiento = dtpFechaNac.Value;
+        //    empleado.Telefono = txtTelefono.Text; 
+        //    empleado.Email = txtEmail.Text;
+
+        //    // Reemplazar estas líneas:
+        //    //empleado.Sucursal = Convert.ToInt32(cboSucursal.SelectedValue);
+        //    //empleado.TipoEmpleado = Convert.ToInt32(cboTipoEmpleado.SelectedValue);
+
+        //    // Por estas líneas:
+        //    empleado.Sucursal = new Sucursal { ID = Convert.ToInt32(cboSucursal.SelectedValue) };
+        //    empleado.TipoEmpleado = new TipoEmpleado { ID = Convert.ToInt32(cboTipoEmpleado.SelectedValue) };
+        //    empleado.FechaIngreso = dtpFechaIng.Value;
+
+        //    string consultaSQL = "SET DATEFORMAT DMY; " +
+        //                         "INSERT INTO EMPLEADOS VALUES " +
+        //                         "(@nombre," +
+        //                         " @apellido," +
+        //                         " @sucursal," +
+        //                         " @tipoEmpleado," +
+        //                         " @dni," +
+        //                         " @telefono," +
+        //                         " @email," +
+        //                         " @fechaNacimiento," +
+        //                         " @fechaIngreso)";
+
+        //    List<Parametro> lEmpleados = new List<Parametro>();
+
+        //    lEmpleados.Add(new Parametro("@nombre", empleado.Nombres));
+        //    lEmpleados.Add(new Parametro("@apellido", empleado.Apellidos));
+        //    lEmpleados.Add(new Parametro("@dni", empleado.DNI));
+        //    lEmpleados.Add(new Parametro("@fechaNacimiento", empleado.FechaNacimiento));
+        //    lEmpleados.Add(new Parametro("@telefono", empleado.Telefono));
+        //    lEmpleados.Add(new Parametro("@email", empleado.Email));
+        //    lEmpleados.Add(new Parametro("@sucursal", empleado.Sucursal));
+        //    lEmpleados.Add(new Parametro("@tipoEmpleado", empleado.TipoEmpleado));
+        //    lEmpleados.Add(new Parametro("@fechaIngreso", empleado.FechaIngreso));
+
+        //    if (accesoDatos.ActualizarBD(consultaSQL, lEmpleados) > 0)
+        //    {
+        //        MessageBox.Show("Empleado cargado con exito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Exclamation,MessageBoxDefaultButton.Button1);
+        //        this.Close();
+        //    }
+        //}
+
         private void btnAceptar_Click(object sender, EventArgs e)
         {
+            if(accion == Modo.VER)
+            {               
+                this.Close();
+            }
+
+            if (accion == Modo.BORRAR)
+            {
+                var confirmResult = MessageBox.Show(
+                    "¿Está seguro que desea eliminar este empleado?",
+                    "Confirmar eliminación",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning,
+                    MessageBoxDefaultButton.Button2
+                );
+
+                if (confirmResult == DialogResult.Yes)
+                {
+                    // Eliminar de la base de datos
+                    string consultaSQL = "DELETE FROM EMPLEADOS WHERE id_empleado = @id";
+                    List<Parametro> parametros = new List<Parametro>
+                    {
+                        new Parametro("@id", oEmpleado.ID)
+                    };
+
+                    if (accesoDatos.ActualizarBD(consultaSQL, parametros) > 0)
+                    {
+                        // Eliminar de la lista de empleados (si existe una lista global o accesible)
+                        // Por ejemplo, si hay una lista estática en algún lado:
+                        // EmpleadoRepositorio.EliminarEmpleadoPorId(oEmpleado.ID);
+
+                        MessageBox.Show("Empleado eliminado con éxito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo eliminar el empleado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    // No hacer nada, solo cerrar el formulario si se desea
+                    // this.Close();
+                }
+                return;
+            }
+
             if (validarDatos())
             {
                 cargarEmpleado();
             }
         }
-
-
     }
 }
